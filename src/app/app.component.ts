b2487ff8cf1c6f35bdab7fff7b4751eeee401893
Bogdana Zadic <bogdana.zadic.bp@nttdata.ro>
2019-10-26 00:15:05 +0300

    detect logo by host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,14 @@ import {ActivatedRoute} from '@angular/router';
 export class AppComponent {
   title = 'PIUG';
   navbarOpen = false;
+  logo = this.getLogo();
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  getLogo() {
+    const location = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    return location ? '../assets/logo.png' : 'assets/logo.png';
   }
 }
