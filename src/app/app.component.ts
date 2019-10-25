3e6e241c8a91b1773c8873fe8494d68968c3e111
Bogdana Zadic <bogdana.zadic.bp@nttdata.ro>
2019-10-26 00:46:05 +0300

    Build rollout testing #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,8 @@ import {ActivatedRoute} from '@angular/router';
 export class AppComponent {
   title = 'PIUG';
   navbarOpen = false;
-  logo = this.getLogo();
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
-  }
-
-  getLogo() {
-    const location = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    return location ? '../assets/logo.png' : 'assets/logo.png';
   }
 }
